75d954611aa4c43c84e79f2b0753cc8b47b599c3
Robert Libotti <robert.libotti@fgv.br>
2019-09-03 09:05:04 -0300

    listagem inicial dos meses

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from 'react'
-//import useGet from './useGet'
-//import usePost from './usePost'
-//import useDelete from './useDelete'
 import Rest from './rest'
 
 const url2 = 'http://httpbin.org/ip'
@@ -11,29 +8,89 @@ const { useGet, usePost, useDelete } = Rest(baseURL)
 
 
 function App() {
-  const data = useGet('movimentacoes/2019-08')
-  const data2 = useGet(url2)
-  const [postData, post] = usePost('movimentacoes/2019-08')
-  const [deleteData, remove] =  useDelete()
+//  const data = useGet('movimentacoes/2019-08')
+//  const data2 = useGet(url2)
+//  const [postData, post] = usePost('movimentacoes/2019-08')
+//  const [deleteData, remove] =  useDelete()
 
-    const saveNew = ()  => {
-    post({ valor: 10, descricao: 'olaaaa'})
+  const data = useGet('meses')
+
+  const saveNew = ()  => {
+    //post({ valor: 10, descricao: 'olaaaa'})
   }
 
   const doRemove = () => {
-    remove('movimentacoes/2019-08/-Lnm_zmgtLioNG228Cs2')
+    //remove('movimentacoes/2019-08/-Lnm_zmgtLioNG228Cs2')
   }
 
   return (
     <div>
-      <h1>MyMoney</h1>
-      { !data.loading && JSON.stringify(data) }
-      { data.loading && <p>loading...</p> }
-      <pre>{ JSON.stringify(data2) }</pre>
-      <button onClick={saveNew}>SALVAR</button>
-      <pre>{ JSON.stringify(postData) }</pre>
-      <button onClick={doRemove}>EXCLUIR</button>
-      <pre>{ JSON.stringify(deleteData) }</pre>
+      <nav className='navbar navbar-light bg-light'>
+        <div className='container'>
+          <a className='navbar-brand'>MyMoney</a>
+        </div>
+      </nav>
+
+      <h2>Adicionar mês</h2>
+      <select>
+        <option value='2019'>2019</option>
+        <option value='2020'>2020</option>
+      </select>
+
+      <select>
+        <option value='01'>01</option>
+        <option value='02'>02</option>
+        <option value='03'>03</option>
+        <option value='04'>04</option>
+        <option value='05'>05</option>
+        <option value='06'>06</option>
+        <option value='07'>07</option>
+        <option value='08'>08</option>
+        <option value='09'>09</option>
+        <option value='10'>10</option>
+        <option value='11'>11</option>
+        <option value='12'>12</option>
+      </select>
+      <button>Adicionar mês</button>
+      {
+        data.loading && <span>carregando...</span>
+      }
+      {
+        !data.loading && (
+          <div className='container'>
+            <table className='table'>
+              <thead>
+                <tr>
+                  <th>Mês</th>
+                  <th>Previsão entrada</th>
+                  <th>Entradas</th>
+                  <th>Previsao saída</th>
+                  <th>Saídas</th>
+                </tr>
+              </thead>
+              <tbody>
+              {
+                Object
+                .keys(data.data)
+                .map(mes => {
+                  return(
+                    <tr key={mes}>
+                      <td>{mes}</td>
+                      <td>{data.data[mes].previsao_entrada}</td>
+                      <td>{data.data[mes].entradas}</td>
+                      <td>{data.data[mes].previsao_saida}</td>
+                      <td>{data.data[mes].saidas}</td>
+                    </tr>
+                  )
+                })
+              }
+                
+              </tbody>
+            </table>
+          </div>
+        )
+      }
+      <pre>{JSON.stringify(data)}</pre>
     </div>
   )
 }
